b32e6d7a6bb56dae6bf1f9e772833dd3a893ee18
UltiNaruto <zenaruto@ymail.com>
2021-03-01 19:37:49 +0100

    Préinitialisation du repository

---
+++ TNTMan/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TNTMan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
