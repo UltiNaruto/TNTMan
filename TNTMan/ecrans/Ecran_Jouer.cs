4b9432cf272292b7de795bd448838e2660fe71e6
UltiNaruto <zenaruto@ymail.com>
2021-03-02 16:21:14 +0100

    Décorruption de la classe Ecran_Jouer

--- TNTMan/ecrans/Ecran_Jouer.cs
+++ TNTMan/ecrans/Ecran_Jouer.cs
@@ -1,29 +1,30 @@
-﻿using SDL2;
+using SDL2;
 using System;
 using System.Drawing;
 using TNTMan.entitees;
+using TNTMan.map;
+using TNTMan.map.blocs;
 
 namespace TNTMan.ecrans
 {
     class Ecran_Jouer : Ecran
     {
+        //Session session = null;
+        Map map = null;
         Joueur joueur = null;
 
         public Ecran_Jouer() : base("Jeu", null)
         {
-<<<<<<< HEAD
             joueur = new Joueur(4, 1.5f, 1.5f);
             map = new Map();
             map.chargerMapParDefaut();
-=======
-
->>>>>>> 38b087b (AJOUTS & MODIFICATIONS DE CLASSES)
         }
 
-        public new void dessinerEcran(IntPtr rendu)
+        // taille bloc 32x32
+        // grille 15x11 (bords inclus)
+        public override void dessinerEcran(IntPtr rendu)
         {
             base.dessinerEcran(rendu);
-<<<<<<< HEAD
             Size resolution = Gfx.getResolution();
             Size tailleGrille = new Size(Map.LARGEUR_GRILLE * Bloc.TAILLE_BLOC, Map.LONGUEUR_GRILLE * Bloc.TAILLE_BLOC);
             Gfx.nettoyerEcran(Color.Green);
@@ -42,19 +43,14 @@ namespace TNTMan.ecrans
 
             Gfx.remplirRectangle((resolution.Width - tailleGrille.Width) / 2 + (int)(joueur.getPosition().X * Bloc.TAILLE_BLOC) - 8, (resolution.Height - tailleGrille.Height) / 2 + (int)(joueur.getPosition().Y * Bloc.TAILLE_BLOC) - 8, 16, 16, 1, joueur.getCouleur(), joueur.getCouleur());
             Gfx.dessinerTexte(5, 5, 18, Color.Black, "J1 - ({0:0.0}, {1:0.0})", joueur.getPosition().X, joueur.getPosition().Y);
-=======
-            Gfx.nettoyerEcran(Color.Green);
->>>>>>> 38b087b (AJOUTS & MODIFICATIONS DE CLASSES)
         }
 
-        public new void gererSouris()
+        public override void gererSouris()
         {
-            base.gererSouris();
         }
 
-        public new void gererTouches(byte[] etats)
+        public override void gererTouches(byte[] etats)
         {
-<<<<<<< HEAD
             if(etats[(int)SDL.SDL_Scancode.SDL_SCANCODE_W] > 0)
             {
                 joueur.deplacer(0.0f, -1.0f);
@@ -72,9 +68,6 @@ namespace TNTMan.ecrans
                 joueur.deplacer(1.0f, 0.0f);
             }
             joueur.mettreAJour(map);
-=======
-            base.gererTouches(etats);
->>>>>>> 38b087b (AJOUTS & MODIFICATIONS DE CLASSES)
         }
     }
 }
