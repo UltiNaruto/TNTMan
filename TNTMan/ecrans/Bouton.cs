0b8b5c447bbdf3b68dd6e093db7b14e28a3afb72
UltiNaruto <zenaruto@ymail.com>
2021-03-16 11:47:12 +0100

    Amélioration des boutons

--- TNTMan/ecrans/Bouton.cs
+++ TNTMan/ecrans/Bouton.cs
@@ -7,6 +7,7 @@ namespace TNTMan.ecrans
     {
         int id;
         Point position;
+        Size taille;
         protected static readonly Color couleur = Color.FloralWhite;
         protected static readonly Color couleurTexte = Color.Black;
         protected static readonly Color couleurSel = Color.LightGoldenrodYellow;
@@ -16,12 +17,25 @@ namespace TNTMan.ecrans
 
         protected Bouton() { }
 
-        public Bouton(int _id, int x, int y, int px, String fmt, params Object[] args)
+        public Bouton(int _id, int x, int y, int w, int h, int px, String fmt, params Object[] args)
         {
+            Size taille_rect_texte = new Size();
+
             id = _id;
             position = new Point(x, y);
             taillePolice = px;
             texte = String.Format(fmt, args);
+            taille_rect_texte = Gfx.getTailleRectangleTexte(texte, taillePolice);
+            if (w < taille_rect_texte.Width)
+                w = taille_rect_texte.Width;
+            if (h < taille_rect_texte.Height)
+                h = taille_rect_texte.Height;
+            taille = new Size(w, h);
+        }
+
+        public Bouton(int _id, int x, int y, int px, String fmt, params Object[] args)
+            : this(_id, x, y, 200, 20, px, fmt, args)
+        {
         }
 
         public int getId()
@@ -31,7 +45,6 @@ namespace TNTMan.ecrans
 
         public bool curseurDans(Point curseur)
         {
-            Size taille = Gfx.getTailleRectangleTexte(texte, taillePolice);
             return curseur.X >= position.X 
                 && curseur.X <= position.X + taille.Width
                 && curseur.Y >= position.Y
@@ -40,9 +53,9 @@ namespace TNTMan.ecrans
 
         public void dessiner(IntPtr rendu, bool sel)
         {
-            Size taille = Gfx.getTailleRectangleTexte(texte, taillePolice);
+            Size taille_rect_texte = Gfx.getTailleRectangleTexte(texte, taillePolice);
             Gfx.remplirRectangle(position.X, position.Y, taille.Width, taille.Height, 1, sel ? couleurSel : couleur, sel ? couleurSelTexte : couleurTexte);
-            Gfx.dessinerTexte(position.X+1, position.Y+1, taillePolice, sel ? couleurSelTexte : couleurTexte, texte);
+            Gfx.dessinerTexte(position.X + (taille.Width / 2 - taille_rect_texte.Width / 2), position.Y+(taille.Height / 2 - taille_rect_texte.Height / 2), taillePolice, sel ? couleurSelTexte : couleurTexte, texte);
         }
     }
 }
